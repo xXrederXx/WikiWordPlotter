f82782a66de82767bb590f701bdc43d2790862f2
xXrederXx <150281252+xXrederXx@users.noreply.github.com>
2024-11-23 17:47:17 +0100

    Refactor Plotter.cs
    Made different functions instead of doing everything in the Initialization

--- Plotter.cs
+++ Plotter.cs
@@ -3,25 +3,39 @@ using ScottPlot;
 namespace WebScraper;
 
 public class Plotter{
-    public Plotter(List<KeyValuePair<string, int>> totalData, int amountToShow){
+    private readonly Plot plot;
+    public Plotter(List<KeyValuePair<string, int>> totalData, int amountToShow)
+    {
         List<KeyValuePair<string, int>> data = totalData.Take(amountToShow).ToList();
-        Plot plot = new();
-        List<Tick> ticks = new List<Tick>();
-        for (int i = 0; i < data.Count; i++)
-        {
-            KeyValuePair<string, int> item = data[i];
-            var scat = plot.Add.Scatter(i, item.Value);
-            
-            ticks.Add(new Tick(i, item.Key));
-        }
+
+        plot = new Plot();
+        List<Tick> ticks = SetUpValuesAndCalcTicks(data);
+        ApplyTicksAndStyle(ticks);
+
+        plot.SavePng("res.png", 1920, 1080);
+
+    }
+
+    private void ApplyTicksAndStyle(List<Tick> ticks)
+    {
         plot.Axes.Bottom.SetTicks(ticks.Select(x => x.Position).ToArray(), ticks.Select(x => x.Label).ToArray());
         plot.Axes.Bottom.TickLabelStyle.Rotation = 45;
         plot.Axes.Bottom.TickLabelStyle.Alignment = Alignment.MiddleLeft;
 
         plot.Axes.Bottom.MinimumSize = 100;
         plot.Axes.Right.MinimumSize = 100;
+    }
 
-        plot.SavePng("res.png", 1920, 1080);
+    private List<Tick> SetUpValuesAndCalcTicks(List<KeyValuePair<string, int>> data)
+    {
+        List<Tick> ticks = new List<Tick>();
+        for (int i = 0; i < data.Count; i++)
+        {
+            KeyValuePair<string, int> item = data[i];
+            plot.Add.Scatter(i, item.Value);
+            ticks.Add(new Tick(i, item.Key));
+        }
 
+        return ticks;
     }
 }
