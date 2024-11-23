e001b810cea06aeb47ee6d2503333ce8b1848db8
xXrederXx <150281252+xXrederXx@users.noreply.github.com>
2024-11-23 17:51:53 +0100

    Refactor Program.cs
    Reorganised the code

--- Program.cs
+++ Program.cs
@@ -1,49 +1,53 @@
-﻿using System.Collections.Concurrent;
+using System.Collections.Concurrent;
 using System.Diagnostics;
 
 namespace WebScraper;
 
-
 static class MainClass
 {
+    private const int AmountOfWordsOnPlot = 100;
     private static readonly ConcurrentDictionary<string, int> WordCounts = new ConcurrentDictionary<string, int>();
     private static readonly UrlHandler urlHandeler = new UrlHandler();
     public static void Main()
     {
         Console.WriteLine("Start App");
 
-
-        processURL("https://de.wikipedia.org/wiki/Parser");
+        ProcessURL("https://de.wikipedia.org/wiki/Parser"); // Just the startup URL you can change this
         
-        System.Console.WriteLine("How Many Websites do you want to count");
+        Console.WriteLine("How Many Websites do you want to count");
+
         int websitesToCout;
-        while(!int.TryParse(Console.ReadLine(), out websitesToCout)){}
+        while(!int.TryParse(Console.ReadLine(), out websitesToCout)){
+            System.Console.WriteLine("Write a number");
+        }
         
         Stopwatch sw = new Stopwatch();
         sw.Start();
+
         Parallel.For(0, websitesToCout, (i) => {
-            processURL(urlHandeler.GetURL());
+            ProcessURL(urlHandeler.GetURL());
         });
+
         sw.Stop();
-        System.Console.WriteLine(sw.ElapsedMilliseconds);
-        List<KeyValuePair<string, int>> sorted = WordCounts.OrderByDescending(x => x.Value).ToList();
-        _ = new Plotter(sorted, 100);
+        Console.WriteLine(sw.ElapsedMilliseconds);
+
+        List<KeyValuePair<string, int>> sortedWordList = WordCounts.OrderByDescending(x => x.Value).ToList();
+
+        _ = new Plotter(sortedWordList, AmountOfWordsOnPlot);
     }
 
-    private static void processURL(string url)
+    private static void ProcessURL(string url)
     {
         if(string.IsNullOrEmpty(url)) return;
 
-        Scraper scrapper = new Scraper(url);
+        Scraper scraper = new Scraper(url); // Scrap the Website
 
-        string wikiText = scrapper.GetWikiText();
 
-        string[] words = TextProcesser.GetWords(wikiText);
-
-        string[] links = scrapper.GetLinks();
-
+        string[] links = scraper.GetLinks(); // Process the links
         urlHandeler.AddURL(links);
 
+        string wikiText = scraper.GetWikiText(); // Process the Words
+        string[] words = TextProcesser.GetWords(wikiText);
         foreach (string word in words)
         {
             WordCounts.AddOrUpdate(word, 1, (key, count) => count + 1); // Thread-safe update
