6c337af20643c1212b3a8d2447c57b0e1bafbfcc
xXrederXx <150281252+xXrederXx@users.noreply.github.com>
2024-12-01 11:32:54 +0100

    Simplifyed the script and bug fixed Whitespace

--- TextProcesser.cs
+++ TextProcesser.cs
@@ -1,5 +1,3 @@
-using System.Diagnostics;
-
 namespace WebScraper;
 
 public static class TextProcesser
@@ -7,14 +5,16 @@ public static class TextProcesser
     public static string[] GetWords(string text)
     {
         string[] words = text
-            .Split([' '], StringSplitOptions.RemoveEmptyEntries) // Split into words
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries) // Split into words
             .Select(word =>
                 new string(word
                     .Where(char.IsLetter) // Keep only letters
                     .ToArray())
                 .ToLower() // Convert back to char array
             )
+            .Where(x => !string.IsNullOrEmpty(x) && !string.IsNullOrWhiteSpace(x))
             .ToArray(); // Convert to a string array
+
         return words;
     }
 }
