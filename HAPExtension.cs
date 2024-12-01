4d01c5f8c6b8e1c91fce9a727a5e3e5dc1c2f2c4
xXrederXx <150281252+xXrederXx@users.noreply.github.com>
2024-12-01 11:28:51 +0100

    Added Docstrings

--- HAPExtension.cs
+++ HAPExtension.cs
@@ -4,14 +4,26 @@ namespace WebScraper;
 
 public static class HAPExtension
 {
-    public static HtmlNode? SelectDivById(HtmlNode node, string id){
-        HtmlNodeCollection htmlNodeCollection = node.SelectNodes($"//div[@id='{id}']");
-        if(htmlNodeCollection is null || htmlNodeCollection.Count == 0){
+    /// <summary>
+    /// This function returns a HtmlNode or null based on if a div with the spesified id was found.
+    /// </summary>
+    /// <param name="node">The base node to search from</param>
+    /// <param name="id">The id to search</param>
+    /// <returns></returns>
+    public static HtmlNode? SelectDivById(HtmlNode node, string id)
+    {
+        HtmlNodeCollection htmlNodeCollection = node.SelectNodes($".//div[@id='{id}']");
+        if (htmlNodeCollection is null || htmlNodeCollection.Count == 0)
+        {
             return null;
         }
         return htmlNodeCollection[0];
     }
 
+    /// <summary>
+    /// This removes all given nodes
+    /// </summary>
+    /// <param name="nodes">A list of all the nodes to remove</param>
     public static void RemoveAllNodes(HtmlNodeCollection? nodes)
     {
         if (nodes is not null)
@@ -24,11 +36,23 @@ public static class HAPExtension
         }
     }
 
-    public static void RemoveDivById(HtmlNode root, string id){
+    /// <summary>
+    /// Searches a div and removes it if found
+    /// </summary>
+    /// <param name="root">The node to search from</param>
+    /// <param name="id">The id to search</param>
+    public static void RemoveDivById(HtmlNode root, string id)
+    {
         SelectDivById(root, id)?.Remove();
     }
 
-    public static void RemoveNodesByXPath(HtmlNode root, string xpath){
+    /// <summary>
+    /// Removes all nodes which are selected by the xpath
+    /// </summary>
+    /// <param name="root">the root node to run the xpath from</param>
+    /// <param name="xpath">The xpath used to search the other nodes</param>
+    public static void RemoveNodesByXPath(HtmlNode root, string xpath)
+    {
         RemoveAllNodes(root.SelectNodes(xpath));
     }
 }
