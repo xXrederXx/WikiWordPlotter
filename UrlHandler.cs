e650a9ac84845c7f5229365665a68013439d22fe
xXrederXx <150281252+xXrederXx@users.noreply.github.com>
2024-12-01 11:34:01 +0100

    Added Logic and Load functionality

--- UrlHandler.cs
+++ UrlHandler.cs
@@ -1,11 +1,30 @@
 using System.Collections.Concurrent;
 
 namespace WebScraper;
-
 public class UrlHandler
 {
-    private readonly ConcurrentDictionary<string, bool> Completed = new();
+    private readonly ConcurrentBag<string> Completed = new();
     private readonly ConcurrentQueue<string> NotCompleted = new();
+
+    public string[] CompletedURLs => Completed.ToArray();
+    public string[] NotCompletedURLs => NotCompleted.ToArray();
+
+    public const string BaseUrl = "https://de.wikipedia.org";
+    public const string RelativeBaseUrl = "/wiki/";
+
+    public void LoadData(string[] completed, string[] notCompleted)
+    {
+
+        foreach (string url in completed)
+        {
+            Completed.Add(url);
+        }
+
+        foreach (string url in notCompleted)
+        {
+            NotCompleted.Enqueue(url);
+        }
+    }
 
     /// <summary>
     /// Fetches the next URL from the NotCompleted queue.
@@ -13,21 +32,27 @@ public class UrlHandler
     /// <returns>The URL if available; otherwise, an empty string.</returns>
     public string GetURL()
     {
-        if (NotCompleted.TryDequeue(out string? url))
+        if (!NotCompleted.TryDequeue(out string? url))
         {
-            Completed.TryAdd(url, true);
-            return url;
+            return string.Empty;
         }
-        return string.Empty;
+
+        if (url.StartsWith(RelativeBaseUrl))
+        {
+            url = BaseUrl + url;
+        }
+
+        Completed.Add(url);
+        return url;
     }
 
     /// <summary>
     /// Adds a single URL to the NotCompleted queue if it's not already processed.
-    /// </summary>
+    /// <summary>
     /// <param name="url">The URL to add.</param>
     public void AddURL(string url)
     {
-        if (!Completed.ContainsKey(url) && !NotCompleted.Contains(url))
+        if (IsUrlValide(url))
         {
             NotCompleted.Enqueue(url);
         }
@@ -37,11 +62,17 @@ public class UrlHandler
     /// Adds multiple URLs to the NotCompleted queue.
     /// </summary>
     /// <param name="urls">An array of URLs to add.</param>
-    public void AddURL(IEnumerable<string> urls)
+    public void AddURLs(IEnumerable<string> urls)
     {
         foreach (string url in urls)
         {
             AddURL(url);
         }
+    }
+
+
+    public bool IsUrlValide(string url)
+    {
+        return !string.IsNullOrEmpty(url) && !url.Contains("Datei:") && (url.StartsWith(BaseUrl) || url.StartsWith(RelativeBaseUrl)) && !Completed.Contains(url) && !NotCompleted.Contains(url);
     }
 }
