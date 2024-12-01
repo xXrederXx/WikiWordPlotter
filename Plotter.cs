52b5b79df8a519645d3689f48b1c58871ae210c7
xXrederXx <150281252+xXrederXx@users.noreply.github.com>
2024-12-01 11:30:01 +0100

    Added Docstring and fixed Formatting

--- Plotter.cs
+++ Plotter.cs
@@ -2,16 +2,41 @@ using ScottPlot;
 
 namespace WebScraper;
 
-public class Plotter{
+public class Plotter
+{
+
+    /// <summary>
+    /// The plot used
+    /// </summary>
     private readonly Plot plot;
+
+    /// <summary>
+    /// This is esentaly a list from 0 to the number of elements plotet
+    /// </summary>
     private readonly List<double> xPositions;
+
+    /// <summary>
+    /// These are all the yValues
+    /// </summary>
     private readonly List<double> yValues;
 
     // settings
+    /// <summary>
+    /// Turn this off or on to show or dont show the line
+    /// </summary>
     private const bool showLine = true;
+
+    /// <summary>
+    /// Decides if it renders in darkmode
+    /// </summary>
     private const bool darkMode = true;
 
-    public Plotter(List<KeyValuePair<string, int>> totalData, int amountToShow)
+    /// <summary>
+    /// This class will plot the given data to a res.png file
+    /// </summary>
+    /// <param name="totalData">All the data, it is a list of key - value pairs, the keys are the words and the values are the times it got counted</param>
+    /// <param name="amountToShow">This is the number of Word, which are shown on the graph</param>
+    public Plotter(List<KeyValuePair<string, int>> totalData, int amountToShow, string imagePath)
     {
         List<KeyValuePair<string, int>> data = totalData.Take(amountToShow).ToList();
 
@@ -21,7 +46,8 @@ public class Plotter{
 
         MakeBars();
 
-        if(showLine){
+        if (showLine)
+        {
             MakeLine();
         }
 
@@ -30,13 +56,17 @@ public class Plotter{
 
         ApplyStyle();
 
-        plot.SavePng("res.png", 1920, 1080);
+        plot.SavePng(imagePath, 1920, 1080);
 
     }
 
+    /// <summary>
+    /// This applys the style of the plot
+    /// </summary>
     private void ApplyStyle()
     {
-        if(darkMode){
+        if (darkMode)
+        {
             ApplyDarkModeStyle();
         }
 
@@ -46,6 +76,9 @@ public class Plotter{
         plot.Axes.Margins(bottom: 0);
     }
 
+    /// <summary>
+    /// This is used to apply a dark mode based style
+    /// </summary>
     private void ApplyDarkModeStyle()
     {
         // change background and text color
@@ -63,6 +96,10 @@ public class Plotter{
         plot.Grid.YAxisStyle.MinorLineStyle.Width = 1;
     }
 
+    /// <summary>
+    /// Makes all the given Ticks appeare on the Graph
+    /// <summary>
+    /// <param name="ticks">The ticks to draw</param>
     private void MakeTicks(List<Tick> ticks)
     {
         plot.Axes.Bottom.SetTicks(ticks.Select(x => x.Position).ToArray(), ticks.Select(x => x.Label).ToArray());
@@ -70,27 +107,39 @@ public class Plotter{
         plot.Axes.Bottom.TickLabelStyle.Alignment = Alignment.MiddleLeft;
     }
 
+    /// <summary>
+    /// This function draws the Bars on to the plot
+    /// </summary>
     private void MakeBars()
     {
-        var barPlot = plot.Add.Bars(xPositions, yValues);
+        ScottPlot.Plottables.BarPlot barPlot = plot.Add.Bars(xPositions, yValues);
         barPlot.Color = new Color("777777");
 
-        foreach (var bar in barPlot.Bars)
+        foreach (Bar bar in barPlot.Bars)
         {
             bar.LineWidth = 0;
         }
 
     }
 
+    /// <summary>
+    /// This can be used to draw a line on the plot
+    /// </summary>
     private void MakeLine()
     {
-        var linePlot = plot.Add.ScatterLine(xPositions.ToArray(), yValues.ToArray(), new("aa5599"));
+        ScottPlot.Plottables.Scatter linePlot = plot.Add.ScatterLine(xPositions.ToArray(), yValues.ToArray(), new("aa5599"));
         linePlot.LineWidth = 2;
         linePlot.Smooth = true;
         linePlot.SmoothTension = 1f;
     }
 
-    private static List<Tick> GenerateTicks(List<KeyValuePair<string, int>> data){
+    /// <summary>
+    /// This function calculates all the Ticks
+    /// </summary>
+    /// <param name="data">The data used to generate the Ticks</param>
+    /// <returns>A list of the calculated ticks</returns>
+    private static List<Tick> GenerateTicks(List<KeyValuePair<string, int>> data)
+    {
         List<Tick> ticks = new List<Tick>();
         for (int i = 0; i < data.Count; i++)
         {
